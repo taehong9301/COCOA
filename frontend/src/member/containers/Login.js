976a4f5fef721737bd30fef86b9aa81585202b87
TaehongKim <public.devhong@gmail.com>
2021-01-20 00:26:32 +0900

    로그인 UI 개발 (#7)

--- frontend/src/member/containers/Login.js
+++ frontend/src/member/containers/Login.js
@@ -1,41 +1,28 @@
 import React from 'react'
+import '../style/login.css'
 
 const Login = () => {
+  const onSubmit = (event) => {
+    event.preventDefault()
+  }
   return (
     <div className="loginWrapper">
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
+      <form onSubmit={onSubmit} className="loginForm">
+        <input
+          type="text"
+          name="email"
+          className="loginInput"
+          placeholder="Enter your email"
+        />
+        <input
+          type="password"
+          name="password"
+          className="loginInput"
+          placeholder="Enter your password"
+        />
+        <button className="loginBtn">LOGIN</button>
+        <button className="signUpBtn">SIGN UP</button>
+      </form>
     </div>
   )
 }
