0245289109f7daa1884b5eeab7ede4490a18ab07
TaehongKim <public.devhong@gmail.com>
2021-01-20 22:18:30 +0900

    로그인 ui/ux 개발 (#7)

--- frontend/src/member/containers/Login.js
+++ frontend/src/member/containers/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../style/login.css'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   const onSubmit = (event) => {
@@ -8,6 +9,7 @@ const Login = () => {
   return (
     <div className="loginWrapper">
       <form onSubmit={onSubmit} className="loginForm">
+        <h3 className="loginTit">회원 로그인</h3>
         <input
           type="text"
           name="email"
@@ -21,8 +23,28 @@ const Login = () => {
           placeholder="Enter your password"
         />
         <button className="loginBtn">LOGIN</button>
-        <button className="signUpBtn">SIGN UP</button>
+
+        <div className="loginHelpMenu">
+          <Link to="/search/email">이메일 찾기</Link>/
+          <Link to="/search/password">비밀번호 찾기</Link>
+        </div>
+
+        <h4 className="oauthLoginTit">간편 로그인</h4>
+        <button className="googleLoginBtn">GOOGLE LOGIN</button>
+        <button className="kakaoLoginBtn">KAKAO LOGIN</button>
+        <button className="naverLoginBtn">NAVER LOGIN</button>
       </form>
+
+      <div className="signUpWrapper">
+        <div className="signUpDesc">
+          <h3 className="signUpTit">아직 회원이 아니신가요?</h3>
+          <p>
+            회원가입을 하시면 회원에게만 제공되는 다양한 혜택과 이벤트에
+            참여하실 수 있습니다. 코코아 회원만의 특별한 혜택을 만나보세요.
+          </p>
+        </div>
+        <button className="signUpBtn">SIGN UP</button>
+      </div>
     </div>
   )
 }
